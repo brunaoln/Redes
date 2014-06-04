b9eb90080004ef7b502df51400a66f60ba1e771b
brunaoln <brunaoln@gmail.com>
2014-06-03 18:21:01 -0600

    Create route.py

---
+++ route.py
@@ -0,0 +1,47 @@
+MAX_ROUTES = 128
+MAX_TTL = 120
+
+def route:
+    dest = 0
+    nexthop = 0
+    cost = 0
+    ttl = 0
+    def __init__():
+        self.dest = 0
+        self.nexthop = 0
+        self.cost = 0
+        self.ttl = 0
+
+def router:
+    table = []
+    num_routes = 0
+    def __init__():
+        for i in range(0, MAX_ROUTES):
+            table.append(route())
+            
+    #dada um nova rota atualiza a tabela de roteamento
+    def merge_routes (new):
+        for (i in range(0, num_routes)):
+            if(new.dest == self.table[i].dest):
+                if(new.cost + 1 < self.table[i].cost):
+                    break
+                else if (new.nexthop == self.nexthop):
+                    break
+                else:
+                    return
+        if (i == num_routes):
+            if (num_routes < MAX_ROUTES):
+                num_routes += 1
+            else:
+                return #tabela ja esta cheia
+        #atualiza a tabela de roteamento
+        self.table[i] = new
+        self.table[i].ttl = MAX_TTL
+        self.table[i].cost += 1
+    
+    def updatingRoutingTable(newRoute, numNewRoutes):
+        for (i in range(0, numNewRoutes):
+            self.merge_routes(newRoute[i])
+    
+        
+        
