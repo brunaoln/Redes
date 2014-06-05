71fdda5bfe4af09173469f8da0f45ef87da8c8b7
inaC <inaciogcerqueira@gmail.com>
2014-06-04 23:06:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,47 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*
+
 import route
 import socket
 import threading
 import time
 from collections import deque
 
-#definir max size para os campos
 TAMANHO_TABELA = 500 
 tabela = deque(maxlen = TAMANHO_TABELA) # create a deque with SIZE_BUFFER positions
-PORT = 9999
+PORT = 9999 #porta com a qual sera feita a conexao
 timeout = 1 # DECLAREI UM VALOR ALEATORIO PARA TIMEOUT
+TEMPO_PARA_REENVIO = 60 #periodo (em segundos) para enviar mensagem de rotina
 
-def le_arquivo(file, tabela):
-	global TAMANHO_TABELA
-	with open(file, "rb") as f:
-		while True: 
-			c = f.read(1350) #1350
-			#se acabou o arquivo
-			if not c:
-				print "End of file\n"
-				exitFlag = 1
-				break
-			#enquanto o buffer estiver cheio
-			while (len(tabela) >= TAMANHO_TABELA):
-				#print("Full Buffer\n")
-				continue
-			while True:
-				try:
-					tabela.appendleft(c)
-					break
-				except:
-					continue
-	return
+
 
 class ThreadEnviaTabela (threading.Thread):
     def __init__():
         threading.Thread.__init__(self)
 
     def run(self):
-	global tabela
- 	
- 	route.
+ 	route.enviaRotina()
+ 	#envia periodicamente seu vetor de distancias
+
+class ThreadRecebeTabela (threading.Thread):
+    def __init__():
+        threading.Thread.__init__(self)
+
+    def run(self):
+ 	route.recebeTabela()
+ 	#recebeTabela vai receber a tabela de um de seus vizinhos, em seguida enviará um ACK, e por fim mandará
+ 	# seu vetor de distâncias atualizado para outros,
 
 
 def main():
     #PORT = parametro de entrada
 	global tabela
     
-    teste = myThread(0, 0, "entrada.pdf", 0, Tam_arq) #esta funcionando
-    teste.start()
+    enviar = ThreadEnviaTabela()
+    enviar.start()
 
-    teste2 = myThread(1, 0, "entrada.pdf", 1, Tam_arq)
-    teste2.start()
+    receber = ThreadRecebeTabela()
+    receber.start()
 
 if __name__ == "__main__":
         main()
