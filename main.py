7939731aa33077c437216451dd491a1cd42f428e
inaC <inaciogcerqueira@gmail.com>
2014-06-06 18:10:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import socket
 import threading
 import time
 import sys
+from netifaces import interfaces, ifaddresses, AF_INET
 #from collections import dequee
 #tabela = route.router()
 tabela = None
@@ -33,17 +34,34 @@ class ThreadRecebeTabela (threading.Thread):
 		tabela.recebeTabela()
  	#recebeTabela vai receber a tabela de um de seus vizinhos, em seguida enviará um ACK, e por fim mandará
  	# seu vetor de distâncias atualizado para outros,
-
+class InterfacesInfo:
+	ifinfo = None
+	addr = None
+	netmask = None
+	def __init__(self, ifinfo, addr, netmask):
+		self.ifinfo = ifinfo
+		self.addr = addr
+		self.netmask = netmask
+	
 
 def main():
     #PORT = parametro de entrada
 	global tabela
+	
+	lista_Interfaces = list()
 	argumentos = sys.argv[1:]
 	host_name = ""
     	for i in xrange(len(argumentos)):
     		if argumentos[i] == '-h':
     			host_name = argumentos[i+1]
     	tabela = route.router(host_name)
+    	
+    	for ifName in interfaces():
+    		ifinfo = ifaddresses(ifName).setdefault(AF_INET)[0]
+    		addr = ifinfo['addr']
+    		netmask = ifinfo['netmask']
+    		aux = InterfacesInfo(ifinfo, addr, netmask)
+    		lista_Interfaces.append(aux)
     	
 	enviar = ThreadEnviaTabela()
 	enviar.start()
